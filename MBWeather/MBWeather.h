cd156d5f7cb82c000247624f6e49bb1e832ac7e9
Mohamed <almostaphasmart@Mohameds-MacBook-Pro.local>
2022-07-30 13:27:03 +0100

    MBWeather Framework: init + creating model & network layer

---
+++ MBWeather/MBWeather.h
@@ -0,0 +1,18 @@
+//
+//  MBWeather.h
+//  MBWeather
+//
+//  Created by Mohamed on 28/07/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MBWeather.
+FOUNDATION_EXPORT double MBWeatherVersionNumber;
+
+//! Project version string for MBWeather.
+FOUNDATION_EXPORT const unsigned char MBWeatherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MBWeather/PublicHeader.h>
+
+
